b1784ea403ab35f18382058d2e4116444ac94b9c
ppbntl19 <ppbntl19@gmail.com>
2015-04-28 13:01:21 +0530

    main.js
    main.js file  for  requiireJs

---
+++ main.js
@@ -0,0 +1,19 @@
+
+define(['app/app', 'app/helper', 'text!../../templates/app/content.about.html'],
+		function (App, Helper, contentTmpl){
+			App.Page_About = (function(){
+					var Page_About = {};
+					var _pageName = 'about';
+					var _pageLayout = Helper.newPageLayout({
+					name:_pageName,
+					panelView: Helper.newPanelView(),
+					headerView: Helper.newHeaderView({name:_pageName, title:'Welcome to the About Page'}),
+					contentView: Helper.newContentView({name:_pageName, template: _.template(contentTmpl, {}) }),
+					footerView: Helper.newFooterView({name:_pageName, title:'About Footer'})
+					});
+					App.addInitializer(function(options){
+			App.vent.trigger("page:about:started");
+		});
+		return Page_About;
+		})();
+return App.Page_About;
